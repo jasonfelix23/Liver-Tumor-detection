fdd8ff42b405c7898569b60c1fa76af88539810d
jasonfelix23 <jasonfelix789@gmail.com>
2022-03-17 13:57:33 +0530

    Nii update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from tensorflow import keras
 from tensorflow.keras.preprocessing import image
 from werkzeug.utils import secure_filename
 import matplotlib.pyplot as plot
-
+import nibabel as nib
 from PIL import Image
 import os
 
@@ -66,6 +66,129 @@ def process_img(img, add_pixels_value=0):
     new_img = cv2.applyColorMap(img, cv2.COLORMAP_INFERNO)
 
     return np.array(new_img, dtype=object)
+
+def read_nii(filepath):
+    ct_scan = nib.load(filepath)
+    array   = ct_scan.get_fdata()
+    array   = np.rot90(np.array(array))
+    return(array)
+
+dicom_windows = types.SimpleNamespace(
+    brain=(80,40),
+    subdural=(254,100),
+    stroke=(8,32),
+    brain_bone=(2800,600),
+    brain_soft=(375,40),
+    lungs=(1500,-600),
+    mediastinum=(350,50),
+    abdomen_soft=(400,50),
+    liver=(150,30),
+    spine_soft=(250,50),
+    spine_bone=(1800,400),
+    custom = (200,60)
+)
+
+@patch
+def windowed(self:Tensor, w, l):
+    px = self.clone()
+    px_min = l - w//2
+    px_max = l + w//2
+    px[px<px_min] = px_min
+    px[px>px_max] = px_max
+    return (px-px_min) / (px_max-px_min)
+
+class TensorCTScan(TensorImageBW): _show_args = {'cmap':'bone'}
+
+@patch
+def freqhist_bins(self:Tensor, n_bins=100):
+    imsd = self.view(-1).sort()[0]
+    t = torch.cat([tensor([0.001]),
+                   torch.arange(n_bins).float()/n_bins+(1/2/n_bins),
+                   tensor([0.999])])
+    t = (len(imsd)*t).long()
+    return imsd[t].unique()
+    
+@patch
+def hist_scaled(self:Tensor, brks=None):
+    if self.device.type=='cuda': return self.hist_scaled_pt(brks)
+    if brks is None: brks = self.freqhist_bins()
+    ys = np.linspace(0., 1., len(brks))
+    x = self.numpy().flatten()
+    x = np.interp(x, brks.numpy(), ys)
+    return tensor(x).reshape(self.shape).clamp(0.,1.)
+    
+    
+@patch
+def to_nchan(x:Tensor, wins, bins=None):
+    res = [x.windowed(*win) for win in wins]
+    if not isinstance(bins,int) or bins!=0: res.append(x.hist_scaled(bins).clamp(0,1))
+    dim = [0,1][x.dim()==3]
+    return TensorCTScan(torch.stack(res, dim=dim))
+
+@patch
+def save_jpg(x:(Tensor), path, wins, bins=None, quality=90):
+    fn = Path(path).with_suffix('.jpg')
+    x = (x.to_nchan(wins, bins)*255).byte()
+    im = Image.fromarray(x.permute(1,2,0).numpy(), mode=['RGB','CMYK'][x.shape[0]==4])
+    im.save(fn, quality=quality)
+
+def processNiifiles(filepath):
+    curr_ct        = read_nii("static/"+filepath)
+    curr_file_name = str(filepath).split('.')[0]
+    curr_dim       = curr_ct.shape[2] # 512, 512, curr_dim
+    curr_count = 0
+    file_array = []
+    for curr_slice in range(int(curr_dim/2),curr_dim,3): 
+        data = tensor(curr_ct[...,curr_slice].astype(np.float32))
+        save_file_name = f'{curr_file_name}_slice_{curr_slice}.jpg'
+        data.save_jpg(f"static/"+save_file_name, [dicom_windows.liver,dicom_windows.custom])
+        curr_count += 1
+        file_array.append(save_file_name)
+        if(curr_count == 4):
+            break;
+    return file_array
+
+@app.route("/nii", methods=["GET", "POST"])
+def nii():
+        
+
+    if request.method == 'POST':
+        for filename in os.listdir('static/'):
+            if filename.startswith('work'):  # not to remove other images
+                os.remove('static/' + filename)
+            if filename.startswith('scan'):
+                os.remove('static/' + filename)
+            if filename.startswith('pred'):
+                os.remove('static/' + filename)
+                
+        file = request.files['nii']
+        if 'nii' not in request.files:
+            return render_template('nii.html', ses=ses, error="File not found!!!")
+
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            return render_template('nii.html', ses=ses, error="File not found!!!")
+        if file:
+            filename = secure_filename(file.filename)
+            timeStamp ="work" + str(time.time()) + ".nii"
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], timeStamp )
+            
+            file.save(filepath)
+            file_array = processNiifiles(timeStamp)
+
+            #print(prediction)
+            return render_template("imagesNii.html",file_array = file_array, ses=ses,name=name, error="")
+    return render_template("nii.html",name= name, ses=ses, error="")
+
+@app.route("/imagesNii", methods=['GET', 'POST'])
+def imagesNii():
+    return render_template('imagesNii.html', ses=ses, error="")
+
+@app.route("/predNii/<filename>", methods=['GET'])
+def predNii(filename):
+    filename = request.args.get('filename')
+    return render_template('imagesNii.html', ses=ses, error="")
 
 
 @app.route("/mainPage", methods=["GET", "POST"])
@@ -148,6 +271,10 @@ def mainPage():
 @app.route("/result", methods=['GET', 'POST'])
 def result():
     return render_template('result.html', predicted_result="From the outer", ses=ses, error="")
+
+
+
+
 
 
 
